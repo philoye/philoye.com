dce19ff1c452d23d8d4d5fdb1ff6c69eaf154d7b
Phil Oye <philoye@philoye.com>
2025-07-13 15:00:55 +1000

    New ruby 3 syntax

--- helpers.rb
+++ helpers.rb
@@ -5,7 +5,7 @@ require 'haml'
 require 'base64'
 
 module Haml::Helpers
-  def svg_use_tag(id, klass={})
+  def svg_use_tag(id, klass)
     haml_tag :svg, :class => klass do
       haml_tag :use, "xlink:href"=>id
     end
@@ -16,7 +16,7 @@ module Haml::Helpers
 		File.read(path)
   end
 
-  def inline_jpeg(filename, width, height, classname='' )
+  def inline_jpeg(filename, width, height, classname )
     path = File.join("src/#{filename}.jpg")
     data = Base64.encode64(open(path).to_a.join)
     haml_tag :img, src: "data:image/jpeg;base64,#{data}", alt: filename, class: classname, width: width, height: height
